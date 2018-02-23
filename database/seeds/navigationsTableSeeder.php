2976a913cf85908f403143124a077f891560d7fd
cmiriyala <cmiriyala@unomaha.edu>
2018-02-23 02:52:45 -0600

    sprint2

---
+++ database/seeds/navigationsTableSeeder.php
@@ -0,0 +1,154 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class navigationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('navigations')->insert([
+            [
+                'navigation_name' => 'History of Present Illness (HPI)',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'Medical History',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'Personal History (PMHx)',
+                'parent_id' =>  2
+            ],
+            [
+                'navigation_name' => 'Family History (FMHx)',
+                'parent_id' =>  2
+            ],
+            [
+                'navigation_name' => 'Surgical History',
+                'parent_id' =>  2
+            ],
+            [
+                'navigation_name' => 'Social History (SHx)',
+                'parent_id' =>  2
+            ],
+            [
+                'navigation_name' => 'Medications',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'Vital Signs',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'Review of System (ROS)',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'Constitutional',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'HENT',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Eyes',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Respiratory',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Cardiovascular',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Musculoskeletal',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Integumentary',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Neurological',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Psychological',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Gastrointestinal',
+                'parent_id' =>  9
+            ],
+            [
+                'navigation_name' => 'Physical Exam',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'Constitutional',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'HENT',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Eyes',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Respiratory',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Cardiovascular',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Musculoskeletal',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Integumentary',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Neurological',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Psychological',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Gastrointestinal',
+                'parent_id' =>  20
+            ],
+            [
+                'navigation_name' => 'Orders',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'Results',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'MDM/Plan',
+                'parent_id' =>  null
+            ],
+            [
+                'navigation_name' => 'Disposition',
+                'parent_id' =>  null
+            ]
+        ]);
+    }
+}
