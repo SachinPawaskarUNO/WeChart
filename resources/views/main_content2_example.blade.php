57519a4bc06ce34ba3294dd79bbce0b06048d354
Tarun Reddy Battula <tarunreddy.battula@gmail.com>
2018-03-11 22:32:55 -0500

    Updated admin adding media after adding defects

---
+++ resources/views/main_content2_example.blade.php
@@ -0,0 +1,30 @@
+Hello
+<table  id="audio_table" class="table table-striped table-bordered table-hover">
+    <thead>
+    <tr class="panel-heading" style="background-color: lightblue;font-size: medium">
+        <th><i aria-hidden="true"></i> Name</th>
+        <th><i aria-hidden="true"></i> Link  </th>
+        <th> </th>
+
+    </tr>
+    </thead>
+    <tbody>
+    @foreach ($audios as $audio)
+        <tr id="edittest">
+            <td id="edittest1"><?php echo($audio->audio_lookup_value_tag); ?> </td>
+            <td id="edittest2"><?php echo($audio->audio_lookup_value_link); ?></td>
+            <!--  <td style="text-align: right" >
+                  <a href="#" class="edit btn enable" id="edit">
+                      <i class="fa fa-edit" aria-hidden="true"></i>
+                  </a>
+              </td> -->
+            <td style="text-align: right">
+                <a href="{{ route('delete_audio', ['id' => $audio->audio_lookup_value_id]) }}" class="btn btn-danger enable" id="delete">
+                    <i class="fa fa-trash-o" aria-hidden="true"></i> Delete
+                </a>
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+{{ $audios->links() }}
